6d1114308e3d9f849e3941da7e686497bb065992
Gerson Saravia <gersonsaravia@hotmail.com>
2011-10-31 23:09:33 -0600

    clase carta actualizada

--- src/Cartas.java
+++ src/Cartas.java
@@ -9,4 +9,20 @@
  */
 public class Cartas {
 
+    protected String color;
+    protected int valor;
+
+    public Cartas(String c, int v){
+        color = c;
+        valor = v;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getValor(){
+        return valor;
+    }
+
 }
